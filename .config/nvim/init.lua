335ffd9093ac615c925ef04ae17e91a7e1003fff
Enrique Plata <>
2023-06-10 13:50:36 +0200

    Adding new new configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,7 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
+--require "user.options"
+--require "user.keymaps"
+--require "user.plugins"
+require("user.core.colorscheme")
+require("user.core.keymaps")
+require("user.core.options")
 
